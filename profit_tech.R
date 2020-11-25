64e880f34260a8db0b852af98f9814fb06c0e49b
qiusiya <qiusiya@Siyas-MacBook-Air.local>
2020-11-26 01:24:31 +0800

    Sensitivity Analysis in "test.rmd" updated all the funstions to take in all parameters instead of using the default value

--- profit_tech.R
+++ profit_tech.R
@@ -1,12 +1,12 @@
 # write a function to calculate the profit for one technology
-profit_tech <- function(E){
-  stock <- stock_dynamic(E)
+profit_tech <- function(r,K,X0, p,c,E,C, q_matrix){
+  stock <- stock_dynamic(r,K,X0, p,c,E,C)
   harvest <- NULL
   catch_by_tech <-  q_matrix*E
   for(i in 1:tech_num){
     tmp <-  catch_by_tech[i,]
     dim(tmp) <- c(species_num,1)
-    tmp_harvest <- (stock %*% (tmp*parameter$p)) - C[i]*E[i]
+    tmp_harvest <- (stock %*% (tmp*p)) - C[i]*E[i]
     harvest <- append(harvest, tmp_harvest)
   }
   x = matrix(harvest,ncol=tech_num,nrow=(year+1))
