a46027e7593a6efb80d13e14db8f31a6e9a3685b
KatLeigh11 <kleigh@ucsb.edu>
2020-11-27 23:10:28 -0800

    sensitivity analysis additions
    added sensitivity runs for all params (not just for E)

--- harvest_tech.R
+++ harvest_tech.R
@@ -2,9 +2,9 @@
 harvest_tech <- function(E){
   stock <- stock_dynamic(E)
   harvest <- NULL
-  catch_by_tech <-  q_matrix*E
+  catch_by_techXspecies_combo <-  q_matrix*E
   for(i in 1:tech_num){
-    tmp <-  catch_by_tech[i,]
+    tmp <-  catch_by_techXspecies_combo[i,]
     dim(tmp) <- c(species_num,1)
     tmp_harvest <- stock %*% tmp
     harvest <- append(harvest, tmp_harvest)
