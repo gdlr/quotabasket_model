26f1abe55dab416b5079004b5bbe8b8b7c06fb3f
KatLeigh11 <kleigh@ucsb.edu>
2020-11-28 20:22:30 -0800

    messing w/ 2 attributes
    everything works above the IGNORE section

--- Kats profit for 2 attribute fish.R
+++ Kats profit for 2 attribute fish.R
@@ -1,13 +1,15 @@
 # write a function to calculate the profit for one technology using 2 diff fish characteristics
 profit_tech <- function(E){
-  stock <- stock_dynamic(E)
+  stock <- stock_dynamic(K = parameter$K, X0 = parameter$X0, p = parameter$p, c = parameter$c, C = C, r = parameter$r, E = E)
   harvest <- NULL
+  ratio <- rat_new
   catch_by_techxspecies_combo <-  q_matrix*E
   for(i in 1:tech_num){
-    tmp <-  catch_by_techxspecies_combo[i,]
+    tmp <-  catch_by_techxspecies_combo[1,]
     dim(tmp) <- c(species_num,1)
     tmp_prop_catch_species_att <- tmp*ratio
-    tmp_prop_rev_species_att <- tmp_prop_catch_species_att*p_new
+    tmp_prop_rev_species_att <- tmp_prop_catch_species_att*p
+    
       stock %*% tmp_prop_catch_species_att
     
     harvest <- append(harvest, tmp_harvest)
