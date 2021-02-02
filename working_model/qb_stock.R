b43ee90dc730a122f24d3a96d00d8cb2fd1f0612
Gabriel De La Rosa <gabrieldelarosa@Gabriels-MacBook-Pro.local>
2021-02-02 14:51:57 -0800

    Fixed loop for multiple baskets. Per species profit not working.

--- working_model/qb_stock.R
+++ working_model/qb_stock.R
@@ -16,9 +16,11 @@ qb_stock <- function(species, tech, cost, baskets, mortality, years){
   # Make a few full data frames we can fill later
   h_df <- data.frame(matrix(ncol = nspecies))
   e_df <- data.frame(matrix(ncol = ntech))
+  
+  ## Profits:
   pft_df <- data.frame(matrix(ncol = ntech))
   pft_sp_df <- data.frame(matrix(ncol = nspecies))
-  horizon <-seq(0, years,by=1) 
+  
   #WWWW
   # We still probably want to calculate harvest per species AND technology...
   #WWWW
@@ -33,10 +35,10 @@ qb_stock <- function(species, tech, cost, baskets, mortality, years){
   diag(D) <- 2*cost_df$cost
   
   # Z is the the tech matrix
-  Z <- as.matrix(tech_df[,-1])
+  Z <- t(as.matrix(tech_df[,-1]))
   
   # P is the price matrix
-  P <- sp_df$p
+  P <- as.matrix(sp_df$p)
   
   # M is whether a species is contained in a basket:
   M <- as.matrix(baskets_df)
@@ -57,7 +59,7 @@ qb_stock <- function(species, tech, cost, baskets, mortality, years){
     # B, where the diagonal corresponds to stock sizes (X) for each species
     B <- matrix(0, nrow = nspecies, ncol = nspecies)
     diag(B) <- as.numeric(tail(stock_df,1)) ### Stock goes here...
-    # d combines the P,B,and Z matrices yeilding the per tech revenue (1xtech)
+    # d combines the P,B,and Z matrices yielding the per tech revenue (1xtech)
     # (1xspecies)x(speciesxspecies)x(speciesxtech)
     d <- t(t(P)%*%B%*%Z)
     # A is the transpose matrix that defines which basket each species is in:
@@ -69,7 +71,7 @@ qb_stock <- function(species, tech, cost, baskets, mortality, years){
     # We need to calculate the basket caps based on the initial BASKET size(not stock size!):
     viz_stock <- rowSums(tail(stock_df,1)) ### And here...
     # Make the first constraint based on mortality:
-    b <- -viz_stock * mortality
+    b <- -M%*%(mortality*t(as.matrix(tail(stock_df, 1))))
     # Make a second constraint so they're all greater than zero:
     b2 <- matrix(0, nrow=ntech, ncol=1)
     # Bind them together
@@ -104,17 +106,18 @@ qb_stock <- function(species, tech, cost, baskets, mortality, years){
     # (1xtech)*(techxspecies) = (1xspecies)
     # profit per tech x catchability
     
-    pft_sp <- pft*Z
-    names(pft_sp) <- names(pft_sp_df)
+    # pft_sp <- pft*Z
     
     h_df <- rbind(h_df, h)
     e_df <- rbind(e_df, E)
     pft_df <- rbind(pft_df, pft)
-    pft_sp_df <- rbind(pft_sp_df, pft_sp)
+    # pft_sp_df <- rbind(pft_sp_df, pft_sp)
     
   }
   
   rownames(stock_df) <- NULL
   colnames(stock_df) <- sp_df$s
-  return(list(stock = stock_df, harvest = h_df, effort = e_df, profit = pft_df, profit_species = pft_sp_df,years=horizon))
+  return(list(stock = stock_df, harvest = h_df, effort = e_df, profit = pft_df))
+  
+  # profit_species = pft_sp_df))
 }
